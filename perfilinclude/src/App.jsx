52690b263c8207dc17f2ddcf065e710098bb2ed5
joaopedro <joaopedrofernandess100@gmail.com>
2024-11-20 19:38:59 -0300

    sexto commit

--- perfilinclude/src/App.jsx
+++ perfilinclude/src/App.jsx
@@ -5,24 +5,18 @@ import Tela2 from './tela2.jsx';
 import Tela3 from './tela3.jsx';
 import Tela4 from './tela4.jsx';
 
-
 function App() {
   return (
-    <Router>
+    <Router basename="/projeto.ps2024.2">
       <Routes>
-        {/* Rota padrão que redireciona para Tela1 */}
-        <Route path="/" element={<Navigate to="/tela1.jsx" />} />
-        
-        <Route path="/tela1.jsx" element={<Tela1 />} />
+        <Route path="/" element={<Navigate to="/tela1" />} />
+        <Route path="/tela1" element={<Tela1 />} />
         <Route path="/tela2.jsx" element={<Tela2 />} />
         <Route path="/tela3.jsx" element={<Tela3 />} />
         <Route path="/tela4.jsx" element={<Tela4 />} />
-       
-       
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
